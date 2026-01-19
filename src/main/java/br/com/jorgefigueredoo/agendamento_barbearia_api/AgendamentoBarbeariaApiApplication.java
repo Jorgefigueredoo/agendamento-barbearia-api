e1f2bff4d064048cb9cb51fe45d29dfe96bb73b9
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-01-19 19:06:54 -0300

    projeto iniciado

---
+++ src/main/java/br/com/jorgefigueredoo/agendamento_barbearia_api/AgendamentoBarbeariaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jorgefigueredoo.agendamento_barbearia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoBarbeariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoBarbeariaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jorgefigueredoo/agendamento_barbearia_api/AgendamentoBarbeariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jorgefigueredoo.agendamento_barbearia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoBarbeariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
