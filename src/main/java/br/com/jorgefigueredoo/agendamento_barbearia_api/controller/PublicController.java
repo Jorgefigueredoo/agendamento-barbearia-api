c4abfd065e269d1301c5a23e74fab88e3159a9cc
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-01-20 14:15:04 -0300

    permissão da rota local host

--- src/main/java/br/com/jorgefigueredoo/agendamento_barbearia_api/controller/PublicController.java
+++ src/main/java/br/com/jorgefigueredoo/agendamento_barbearia_api/controller/PublicController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping
 public class PublicController {
