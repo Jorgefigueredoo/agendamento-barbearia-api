6ce4a30fe0be7bc3935a08ee54059557faded21a
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-01-20 09:48:18 -0300

    Barber model feito

---
+++ src/main/java/br/com/jorgefigueredoo/agendamento_barbearia_api/model/Barber.java
@@ -0,0 +1,44 @@
+package br.com.jorgefigueredoo.agendamento_barbearia_api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "barbers")
+public class Barber {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120)
+    private String name;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+
+    public Barber() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
