a74f9ded4d6ed646a7b44e6899f7b507fe95cfdc
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-01-21 16:59:56 -0300

    Login Response DTO

---
+++ src/main/java/br/com/jorgefigueredoo/agendamento_barbearia_api/dto/LoginResponse.java
@@ -0,0 +1,15 @@
+package br.com.jorgefigueredoo.agendamento_barbearia_api.dto;
+
+public class LoginResponse {
+
+    private String token;
+
+    public LoginResponse() {}
+
+    public LoginResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+}
